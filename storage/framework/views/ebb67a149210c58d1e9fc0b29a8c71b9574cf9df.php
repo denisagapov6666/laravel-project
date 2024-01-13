663fc4ebba91e962551e93d3c381162d00544e4f
denisagapo6666 <denisagapov6666@gmail.com>
2024-01-13 21:34:41 +0100

    color picker change

--- storage/framework/views/ebb67a149210c58d1e9fc0b29a8c71b9574cf9df.php
+++ storage/framework/views/ebb67a149210c58d1e9fc0b29a8c71b9574cf9df.php
@@ -1,7 +1,6 @@
-@extends('backend.layouts.app')
-@section('title', __('labels.backend.tests.title').' | '.app_name())
+<?php $__env->startSection('title', __('labels.backend.tests.title').' | '.app_name()); ?>
 
-@push('after-styles')
+<?php $__env->startPush('after-styles'); ?>
     <style>
         .select2-container--default .select2-selection--single {
             height: 35px;
@@ -16,8 +15,8 @@
         }
 
     </style>
-@endpush
-@section('content')
+<?php $__env->stopPush(); ?>
+<?php $__env->startSection('content'); ?>
     <link href = "https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css"
          rel = "stylesheet">
 
@@ -26,77 +25,97 @@
     <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
     <script src="https://cdn.ckeditor.com/4.6.2/standard-all/ckeditor.js"></script>
 
-    {!! Form::open(['method' => 'POST', 'route' => ['admin.tests.store']]) !!}
+    <?php echo Form::open(['method' => 'POST', 'route' => ['admin.tests.store']]); ?>
+
 
     <div class="card">
         <div class="card-header">
-            <h3 class="page-title float-left mb-0">@lang('labels.backend.tests.create')</h3>
+            <h3 class="page-title float-left mb-0"><?php echo app('translator')->get('labels.backend.tests.create'); ?></h3>
             <div class="float-right">
                 <a href="javascript:history.back();"
-                   class="btn btn-success">@lang('labels.backend.tests.view')</a>
+                   class="btn btn-success"><?php echo app('translator')->get('labels.backend.tests.view'); ?></a>
             </div>
         </div>
         <div class="card-body">
             <div class="row">
                 <div class="col-12 col-lg-6 form-group">
-                    {!! Form::label('course_id', trans('labels.backend.tests.fields.course'), ['class' => 'control-label']) !!}
-                    {!! Form::select('course_id', $courses, request('course_id', old('course_id')), ['class' => 'form-control select2']) !!}
+                    <?php echo Form::label('course_id', trans('labels.backend.tests.fields.course'), ['class' => 'control-label']); ?>
+
+                    <?php echo Form::select('course_id', $courses, request('course_id', old('course_id')), ['class' => 'form-control select2']); ?>
+
                 </div>
 
                 <div class="col-12 col-lg-6  form-group">
-                    {!! Form::label('title',trans('labels.backend.tests.fields.title'), ['class' => 'control-label']) !!}
-                    {!! Form::text('title', old('title'), ['class' => 'form-control', 'placeholder' => trans('labels.backend.tests.fields.title')]) !!}
+                    <?php echo Form::label('title',trans('labels.backend.tests.fields.title'), ['class' => 'control-label']); ?>
+
+                    <?php echo Form::text('title', old('title'), ['class' => 'form-control', 'placeholder' => trans('labels.backend.tests.fields.title')]); ?>
+
 
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-12 form-group">
-                    {!! Form::label('description',trans('labels.backend.tests.fields.description'), ['class' => 'control-label']) !!}
-                    {!! Form::textarea('description', old('description'), ['class' => 'form-control ', 'placeholder' => trans('labels.backend.tests.fields.description')]) !!}
+                    <?php echo Form::label('description',trans('labels.backend.tests.fields.description'), ['class' => 'control-label']); ?>
+
+                    <?php echo Form::textarea('description', old('description'), ['class' => 'form-control ', 'placeholder' => trans('labels.backend.tests.fields.description')]); ?>
+
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-12 form-group">
-                    {!! Form::label('text1', trans('labels.backend.tests.fields.text1'), ['class' => 'control-label']) !!}
-                    {!! Form::textarea('text1', "&nbsp", ['class' => 'form-control ckeditor', 'placeholder' => '','name'=>'text1','id' => 'text1']) !!}
+                    <?php echo Form::label('text1', trans('labels.backend.tests.fields.text1'), ['class' => 'control-label']); ?>
+
+                    <?php echo Form::textarea('text1', "&nbsp", ['class' => 'form-control ckeditor', 'placeholder' => '','name'=>'text1','id' => 'text1']); ?>
+
                 </div>
             </div>
             <div class="row">
                 <div class="col-12 form-group">
-                    {!! Form::label('text2', trans('labels.backend.tests.fields.text2'), ['class' => 'control-label']) !!}
-                    {!! Form::textarea('text2',"&nbsp", ['class' => 'form-control ckeditor', 'placeholder' => '','name'=>'text2','id' => 'text2']) !!}
+                    <?php echo Form::label('text2', trans('labels.backend.tests.fields.text2'), ['class' => 'control-label']); ?>
+
+                    <?php echo Form::textarea('text2',"&nbsp", ['class' => 'form-control ckeditor', 'placeholder' => '','name'=>'text2','id' => 'text2']); ?>
+
                 </div>
             </div>
             <div class="row">
                 <div class="col-12 col-lg-6 form-group">
-                    {!! Form::label('color1',trans('labels.backend.tests.fields.color1'), ['class' => 'control-label']) !!}
-                    {!! Form::color('color1', old('color1','#0000ff'), ['class' => 'form-control ']) !!}
+                    <?php echo Form::label('color1',trans('labels.backend.tests.fields.color1'), ['class' => 'control-label']); ?>
+
+                    <?php echo Form::color('color1', old('color1','#0000ff'), ['class' => 'form-control ']); ?>
+
                 </div>
                 <div class="col-12 col-lg-6 form-group">
-                    {!! Form::label('color2',trans('labels.backend.tests.fields.color2'), ['class' => 'control-label']) !!}
-                    {!! Form::color('color2', old('color2',"#808080"), ['class' => 'form-control ']) !!}
+                    <?php echo Form::label('color2',trans('labels.backend.tests.fields.color2'), ['class' => 'control-label']); ?>
+
+                    <?php echo Form::color('color2', old('color2',"#808080"), ['class' => 'form-control ']); ?>
+
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-12 form-group">
-                    {!! Form::hidden('published', 0) !!}
-                    {!! Form::checkbox('published', 1, false, []) !!}
-                    {!! Form::label('published', trans('labels.backend.tests.fields.published'), ['class' => 'control-label font-weight-bold']) !!}
+                    <?php echo Form::hidden('published', 0); ?>
+
+                    <?php echo Form::checkbox('published', 1, false, []); ?>
+
+                    <?php echo Form::label('published', trans('labels.backend.tests.fields.published'), ['class' => 'control-label font-weight-bold']); ?>
+
                 </div>
             </div>
         </div>
     </div>
 
-    {!! Form::submit(trans('strings.backend.general.app_save'), ['class' => 'btn btn-danger']) !!}
-    {!! Form::close() !!}
+    <?php echo Form::submit(trans('strings.backend.general.app_save'), ['class' => 'btn btn-danger']); ?>
+
+    <?php echo Form::close(); ?>
+
 
     <script>
         CKEDITOR.replace('text1', {
             height : 150,
-            filebrowserUploadUrl: `{{route('admin.questions.editor_fileupload',['_token' => csrf_token() ])}}`,
+            filebrowserUploadUrl: `<?php echo e(route('admin.questions.editor_fileupload',['_token' => csrf_token() ])); ?>`,
             filebrowserUploadMethod: 'form',
             extraPlugins: 'font,widget,colorbutton,colordialog,justify',
             startupFocus: true
@@ -104,7 +123,7 @@
         
         CKEDITOR.replace('text2', {
             height : 150,
-            filebrowserUploadUrl: `{{route('admin.questions.editor_fileupload',['_token' => csrf_token() ])}}`,
+            filebrowserUploadUrl: `<?php echo e(route('admin.questions.editor_fileupload',['_token' => csrf_token() ])); ?>`,
             filebrowserUploadMethod: 'form',
             extraPlugins: 'font,widget,colorbutton,colordialog,justify',
             startupFocus: true,
@@ -150,6 +169,8 @@
             CKEDITOR.instances["text2"].setData(cont);
         })
     </script>
-@stop
+<?php $__env->stopSection(); ?>
 
 
+
+<?php echo $__env->make('backend.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\04_01_24\resources\views/backend/tests/create.blade.php ENDPATH**/ ?>
