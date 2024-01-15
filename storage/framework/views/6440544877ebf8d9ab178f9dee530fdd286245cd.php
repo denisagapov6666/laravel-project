d215f8d5b7ca77e174a9c83e4245ef6ce7b26dce
denisagapo6666 <denisagapov6666@gmail.com>
2024-01-15 08:55:39 +0100

    contact area change

--- storage/framework/views/6440544877ebf8d9ab178f9dee530fdd286245cd.php
+++ storage/framework/views/6440544877ebf8d9ab178f9dee530fdd286245cd.php
@@ -10,7 +10,10 @@
                         <div class="contact-left-content ">
                             <div class="section-title mb45 headline text-left">
                                 <h3 class = "text-uppercase"><span><?php echo app('translator')->get('labels.frontend.layouts.partials.contact_us'); ?></span></h3>
-                                <span class="subtitle ml42"><?php echo app('translator')->get('labels.frontend.layouts.partials.get_in_touch'); ?></span>
+                                <h4>
+
+                                    <span class="text-weight-blod"><?php echo app('translator')->get('labels.frontend.layouts.partials.get_in_touch'); ?></span>
+                                </h4>
                                 <!-- <p>
                                     <?php echo e($contact_data["short_text"]["value"]); ?>
 
@@ -22,21 +25,13 @@
                                     <div class="contact-address-details">
 
                                         <div class="address-icon relative-position text-center float-left">
-                                            <i class="fas fa-map-marker-alt"></i>
+                                            <i class="fas fa-phone"></i>
                                         </div>
                                         <div class="address-details ul-li-block">
                                             <ul>
                                                 <?php if($contact_data["primary_address"]["status"] == 1): ?>
                                                     <li>
-                                                        <span><?php echo app('translator')->get('labels.frontend.layouts.partials.primary'); ?>: </span><?php echo e($contact_data["primary_address"]["value"]); ?>
-
-                                                    </li>
-                                                <?php endif; ?>
-
-                                                <?php if($contact_data["secondary_address"]["status"] == 1): ?>
-                                                    <li>
-                                                        <span><?php echo app('translator')->get('labels.frontend.layouts.partials.second'); ?>: </span><?php echo e($contact_data["secondary_address"]["value"]); ?>
-
+                                                        <span><?php echo app('translator')->get('labels.frontend.layouts.partials.primary'); ?></span>
                                                     </li>
                                                 <?php endif; ?>
                                             </ul>
@@ -44,30 +39,6 @@
                                     </div>
                                 <?php endif; ?>
 
-                                <?php if(($contact_data["primary_phone"]["status"] == 1) || ($contact_data["secondary_phone"]["status"] == 1)): ?>
-                                    <div class="contact-address-details">
-                                        <div class="address-icon relative-position text-center float-left">
-                                            <i class="fas fa-phone"></i>
-                                        </div>
-                                        <div class="address-details ul-li-block">
-                                            <ul>
-                                                <?php if($contact_data["primary_phone"]["status"] == 1): ?>
-                                                    <li>
-                                                        <span><?php echo app('translator')->get('labels.frontend.layouts.partials.primary'); ?>: </span><?php echo e($contact_data["primary_phone"]["value"]); ?>
-
-                                                    </li>
-                                                <?php endif; ?>
-
-                                                <?php if($contact_data["secondary_phone"]["status"] == 1): ?>
-                                                    <li>
-                                                        <span><?php echo app('translator')->get('labels.frontend.layouts.partials.second'); ?>: </span><?php echo e($contact_data["secondary_phone"]["value"]); ?>
-
-                                                    </li>
-                                                <?php endif; ?>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                <?php endif; ?>
 
                                 <?php if(($contact_data["primary_email"]["status"] == 1) || ($contact_data["secondary_email"]["status"] == 1)): ?>
 
@@ -79,17 +50,13 @@
                                             <ul>
                                                 <?php if($contact_data["primary_email"]["status"] == 1): ?>
                                                     <li>
-                                                        <span><?php echo app('translator')->get('labels.frontend.layouts.partials.primary'); ?>: </span><?php echo e($contact_data["primary_email"]["value"]); ?>
+                                                        <span><?php echo app('translator')->get('labels.frontend.layouts.partials.second'); ?></span>
+                                                        <br><?php echo e($contact_data["primary_email"]["value"]); ?>
 
                                                     </li>
                                                 <?php endif; ?>
 
-                                                <?php if($contact_data["secondary_email"]["status"] == 1): ?>
-                                                    <li>
-                                                        <span><?php echo app('translator')->get('labels.frontend.layouts.partials.second'); ?>: </span><?php echo e($contact_data["secondary_email"]["value"]); ?>
-
-                                                    </li>
-                                                <?php endif; ?>
+                                               
                                             </ul>
                                         </div>
                                     </div>
@@ -101,12 +68,19 @@
                         </div> -->
                     </div>
                     <!-- <?php if($contact_data["location_on_map"]["status"] == 1): ?> -->
-                        <div class="col-md-3">
+                        <div class="col-md-3 link-website">
                             <!-- <div id="contact-map" class="contact-map-section">
                                 <?php echo $contact_data["location_on_map"]["value"]; ?>
 
                             </div> -->
-                            ;lkajdf;lakjfd;alks
+                            <a href="https://www.facebook.com" target="_blank" rel="noopener" aria-label="React on Facebook" class="hover:text-primary dark:text-primary-dark"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="4em" height="4em" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 2C6.477 2 2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.879V14.89h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.989C18.343 21.129 22 16.99 22 12c0-5.523-4.477-10-10-10z"></path></svg></a>
+                            <a href="https://www.linkedin.com" target="_blank" rel="noopener" aria-label="LinkedIn" class="hover:text-primary dark:text-primary-dark">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="4em" height="4em" fill="currentColor">
+                                    <path fill="none" d="M0 0h24v24H0z"></path>
+                                    <path d="M20 2H4a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zM8.75 19.5h-2.5v-8h2.5v8zm-1.25-9.5a1.75 1.75 0 1 1 0-3.5 1.75 1.75 0 0 1 0 3.5zM19 19.5h-2.5v-4.72c0-1.06-.94-1.78-2-1.78s-2 .72-2 1.78v4.72H9.75v-8h2.25v1.21a3.49 3.49 0 0 1 3-1.57c2 0 3.5 1.59 3.5 4.5v4.86z"></path>
+                                </svg>
+                            </a>
+
                         </div>
                     <!-- <?php endif; ?> -->
                 <!-- <?php else: ?>
